6c3b51421ee4cca0df3dbc2d810d5583822caffd
shiyam129 <32337552+shiyam129@users.noreply.github.com>
2017-12-19 22:44:27 +0700

    Add files via upload

---
+++ Tugas_Gui/src/tugas_gui/JOptionPanel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_gui;
+
+/**
+ *
+ * @author thosiba c55-b5101
+ */
+class JOptionPanel {
+    
+}
